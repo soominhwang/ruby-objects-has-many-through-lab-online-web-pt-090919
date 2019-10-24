8e77807706c700fd8025b2a4ebb888bad9d67806
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-24 03:11:27 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre
+  attr_accessor :name
+
+  @@all=[]
+
+  def initialize(name)
+    @name=name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+#    Song.all.select do |song|
+#      song.genre==self
+#    end
+  end
+
+  def artists
+  end
+
+end
