aef9efd5dcaf27a098013a1e95b5887d84f98c86
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-24 04:01:57 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,19 @@
 class Appointment
   @@all = []
 
+  def initialize (name)
+    @name=name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def patients
+  end
+
+  def doctor
+  end 
+
 end
